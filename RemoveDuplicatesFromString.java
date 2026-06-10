66d61d2596b86136d85bf0988631322995ff07a5
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2026-06-10 23:11:01 +0530

    Add files via upload

---
+++ RemoveDuplicatesFromString.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Example:
+
+String Input: experience
+Output : expinc
+
+*/
+
+import java.util.Scanner;
+
+public class RemoveDuplicatesFromString{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        String temp="";
+        for(int i=0;i<str.length();i++){
+            if(temp.indexOf(str.charAt(i))==-1){
+                temp+=str.charAt(i);
+            }
+        }
+        System.out.println(temp);
+    }
+}
