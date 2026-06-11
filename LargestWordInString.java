62782d7ff88ca005131b740194858fe8c70d40b3
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2026-06-12 01:31:52 +0530

    Add files via upload

---
+++ LargestWordInString.java
@@ -0,0 +1,19 @@
+
+
+
+import java.util.*;
+
+public class LargestWordInString{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        String[] arr=str.split(" ");
+        String largest="";
+        for(int i=0;i<arr.length;i++){
+            if(arr[i].length()>largest.length()){
+                largest=arr[i];
+            }
+        }
+        System.out.println(largest);
+    }
+}
